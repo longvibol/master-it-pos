f9fb68b261336334be829973923ea63be441bfd6
longvibol <vibollong@gmail.com>
2025-09-18 16:51:38 +0700

    week23

---
+++ app/imports/ui/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    name <input type="text" v-model="name"><br>
+    price <input type="text" v-model="price"><br>
+    quantity <input type="text" v-model="qty"><br>
+    amount: {{amount}} <br>
+    <button @click="handleCompute">Submit</button>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+data(){
+  return{
+    name:null,
+    price:0,
+    qty:0,
+    amount:0
+  }
+},
+computed:{
+  
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ app/imports/ui/components/MyComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h4>this is Component</h4>
+  </div>
+</template>
+
+<script>
+export default {
+
+    beforeDestroy(){
+        console.log("Before Destroyed is Component");
+    },
+    destroyed(){
+        console.log("Destroyed is Component");
+    }
+
+}
+</script>
+
+<style>
+
+</style>
